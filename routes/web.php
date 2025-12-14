a250eff9a62702c549e6e1b9a7e0b9e1fa4e33b8
nailahfauziyyah <nailah5105@gmail.com>
2025-12-14 22:07:11 +0700

    nailah-peminjaman

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PeminjamanController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('peminjaman')->group(function () {
+    Route::get('/', [PeminjamanController::class, 'index'])->name('peminjaman.index');
+    Route::get('/create', [PeminjamanController::class, 'create'])->name('peminjaman.create');
+    Route::post('/', [PeminjamanController::class, 'store'])->name('peminjaman.store');
+    Route::get('/{id}', [PeminjamanController::class, 'show'])->name('peminjaman.show');
+    Route::get('/{id}/edit', [PeminjamanController::class, 'edit'])->name('peminjaman.edit');
+    Route::put('/{id}', [PeminjamanController::class, 'update'])->name('peminjaman.update');
+    Route::delete('/{id}', [PeminjamanController::class, 'destroy'])->name('peminjaman.destroy');
 });
