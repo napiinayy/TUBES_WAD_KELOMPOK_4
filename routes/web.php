cd87a9742496ff3ef808bcaeb81d644733b0e353
Tubagus Muchammad Dzaky Nashrullah Sutalaksana <nasrulahdzaky@gmail.com>
2025-12-14 18:50:02 +0700

    kategori CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\KategoriController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::prefix('test-admin')->group(function () {
+    Route::resource('kategori', KategoriController::class);
 });
